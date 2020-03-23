96da18f9b8c59e7b8490f9eb5dcc3db6504da454
Alex Corrêa <alexxx.nada@gmail.com>
2020-03-23 10:20:09 -0300

    Exercício adicional 01

---
+++ semana02/exercicios/fatura.py
@@ -0,0 +1,8 @@
+nome = input("Digite o nome do cliente: ")
+vencimentoDia = input("Digite o dia de vencimento: ")
+vencimentoMes = input("Digite o mês de vencimento: ")
+fatura = input("Digite o valor da fatura: ")
+
+print ("Olá,", nome)
+print ("A sua fatura com vencimento em", vencimentoDia, "de", vencimentoMes, "no valor de R$", fatura, "está fechada.")
+
