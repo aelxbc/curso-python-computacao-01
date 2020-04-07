08f01e940668791821b7b8eed0dc8966d958d72f
Alex Corrêa <alexxx.nada@gmail.com>
2020-04-07 19:52:08 -0300

    Exercício 03 - Vogais

---
+++ semana05/exercicios/vogais.py
@@ -0,0 +1,13 @@
+def vogal(c):
+    if c == "a" or c == "A":
+        return True
+    elif c == "e" or c == "E":
+        return True
+    if c == "i" or c == "I":
+        return True
+    if c == "o" or c == "O":
+        return True
+    if c == "u" or c == "U":
+        return True
+    else:
+        return False
